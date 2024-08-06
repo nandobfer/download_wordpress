83c481ab50a98e5840b9f525466b356651ae56c3
nandobfer <nandobfer@yahoo.com.br>
2024-08-06 14:17:19 -0300

    uncommented

--- download_wordpress.ps1
+++ download_wordpress.ps1
@@ -36,14 +36,14 @@ if (Test-Path -Path $sshkey) {
         
         ssh -i $sshkey $ssh_profile "mysqldump $dbName > database.sql"
 
-        # Write-Output "Comprimindo site para o domínio: $site_domain"
-        # ssh $ssh_profile -i $sshkey "zip -r wordpress.zip /home/$site_domain/$sub_dir ~/database.sql"
+        Write-Output "Comprimindo site para o domínio: $site_domain"
+        ssh $ssh_profile -i $sshkey "zip -r wordpress.zip /home/$site_domain/$sub_dir ~/database.sql"
 
-        # Write-Output "Baixando zip"
-        # scp -i $sshkey "${ssh_profile}:~/wordpress.zip" "."
+        Write-Output "Baixando zip"
+        scp -i $sshkey "${ssh_profile}:~/wordpress.zip" "."
 
-        # Write-Output "Excluindo zip do servidor"
-        # ssh $ssh_profile -i $sshkey "rm -rf ~/wordpress.zip ~/database.sql"
+        Write-Output "Excluindo zip do servidor"
+        ssh $ssh_profile -i $sshkey "rm -rf ~/wordpress.zip ~/database.sql"
     }
 } else {
     Write-Output "Chave SSH não encontrada, peça pro Mizael."
